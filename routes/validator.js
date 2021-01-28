66f5b63848698f69540a6795fcebe8862b535cf4
Timothy-py <adeyeyetimothy33@gmail.com>
2021-01-28 01:52:29 +0100

    added 2 validations

--- routes/validator.js
+++ routes/validator.js
@@ -1,3 +1,5 @@
+const { json } = require('express');
+
 const router = require('express').Router();
 
 // GET: base route
@@ -18,6 +20,32 @@ router.get('/', (req, res)=>{
 
 // POST: rule validation route
 router.post('/validate-rule', (req, res) => {
+
+    let req_obj = req.body
+
+    // check to see if req obj contain only two keys
+    if(Object.keys(req_obj).length != 2){
+        res.status(400).json({
+            message: "The JSON data passed has more or less keys than expected: Expected(2)",
+            status: "error",
+            data: {
+                isValidForRule: true
+            }
+        })
+    }
+    
+    // check if the req obj contain the expected keys: rule and data
+    if(!('rule' in req_obj) || !('data' in req_obj)){
+        res.status(400).json({
+            message: "The JSON data passed does not contain the expected keys: Expected(rule, data)",
+            status: "error",
+            data: {
+                isValidForRule: true
+            }
+        })
+    }
+
+
     res.status(200).json({
         message: "VALIDATION ROUTE"
     })
