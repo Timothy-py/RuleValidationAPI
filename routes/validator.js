367272e29022ef2bd14c7b759450e1776b608759
Timothy-py <adeyeyetimothy33@gmail.com>
2021-01-28 21:18:32 +0100

    done with rule field

--- routes/validator.js
+++ routes/validator.js
@@ -84,23 +84,64 @@ router.post('/validate-rule', (req, res) => {
     }
 
     // conditional: to check if rule.field is a String value
-    if(typeof(req_obj.rule.field) == String){
+    if(typeof(req_obj.rule.field) === "string"){
         // check if the string is a single value or nested object string
         let rule_field = req_obj.rule.field
 
         // check if it's a single value string
-        if(rule_field.split(".").length == 1){  
+        if(rule_field.split(".").length == 1){
+            // get rule.field data: One Level  
+            let first_level = rule_field.split(".")[0]
 
-            // CONTINUE OPERATION HERE
+            // check if 'first_level' is in data object
+            if(first_level in req_obj.data){
+                
+                // CONTINUE OPERATION
+
+            }else{
+                res.status(400).json({
+                    message: `field ${first_level} is missing from data`,
+                    status: "error",
+                    data: null
+                })
+            }
         } 
+
         // check if it's a two levels nested string
         else if(rule_field.split(".").length == 2){
-            // get the two levels key
+            // get rule.field data: Two levels
             let first_level = rule_field.split(".")[0]
             let second_level = rule_field.split(".")[1]
 
-            // CONTINUE OPEARTION HERE
+            // check if first_level exist in data object
+            if(first_level in req_obj.data){
+                // check if second_level exist in data object
+                if(second_level in req_obj.data.first_level){
 
+                    // CONTINUE OPERATION
+
+                }else{
+                    res.status(400).json({
+                        message: `field ${second_level} is missing from data`,
+                        status: "error",
+                        data: null
+                    })    
+                }
+
+            }else{
+                res.status(400).json({
+                    message: `field ${first_level} is missing from data`,
+                    status: "error",
+                    data: null
+                })
+            }
+
+        }else if(rule_field.split(".").length > 2){
+            res.status(400).json({
+                message: "Invalid JSON payload passed.",
+                status: "error",
+                data: null
+            })
         }
     }else{
         res.status(400).json({
@@ -110,9 +151,9 @@ router.post('/validate-rule', (req, res) => {
         })
     }
 
-    res.status(200).json({
-        message: "VALIDATION ROUTE"
-    })
+    // res.status(200).json({
+    //     message: "VALIDATION ROUTE"
+    // })
 
 })
 
